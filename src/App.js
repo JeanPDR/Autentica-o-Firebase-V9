3ae3ed1637d539f0326078b3ee242a38dd4f51f2
Jean Pereira dos Reis <jeanpereira.jpdr@gmail.com>
2023-02-11 10:17:21 +0000

    UPDATE - Organizando Paginas do Projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Home from './Home.jsx';
-import Signup from './SignUp.jsx';
-import SignIn from './SignIn.jsx';
+import Home from './pages/Home/';
+import Login from './pages/Login/';
+import Register from './pages/Register';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 
@@ -13,8 +13,8 @@ function App() {
           <Routes>
             {' '}
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </section>
       </div>
